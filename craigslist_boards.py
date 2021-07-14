9b33cf686609d14ff70ca81a52230bc72cd52bac
SentientHydrogen <timothynmoynihan@gmail.com>
2021-07-14 17:48:30 -0400

    added result_range to the loop so that the results stay within the established area. The results containers would continue to list for results that are farther away

--- craigslist_boards.py
+++ craigslist_boards.py
@@ -33,7 +33,7 @@ print(len(containers))
 x =0
 container = containers[x] #issue here for some reason
 
-print(containers[0])
+# print(containers[0])
 
 # print(container)
 
@@ -68,77 +68,86 @@ print(containers[0])
 
 # print(board_URL)
 
+#Board range capture. to ensure the surfboard range stays in the RI area:
+
+result_range_container = page_soup.find("div", {"class":"search-legend"})
+
+print(result_range_container)
+
+result_range_line = result_range_container.find("span", {"class":"totalcount"})
+
+result_range = result_range_line.text
+
+print(result_range)
+
 
 # print(board_location)
-#grabbing each product
-# filename = "Surfboards.csv"
+# grabbing each product
+filename = "Surfboards.csv"
 
-# f = open(filename, "w")
+f = open(filename, "w")
 
-# headers = "board_title, board_price, board_location, board_URL\n"
+headers = "board_title, board_price, board_location, board_URL\n"
 
-# f.write(headers)
+f.write(headers)
 
 
-# for contianer in containers:
+for contianer in containers[0:int(result_range)]:
     
-#     if container.div.h3.a.text is not None:
-#         preboard_title = container.div.h3.a.text
-#         board_title = preboard_title.strip()
+    if container.div.h3.a.text is not None:
+        preboard_title = container.div.h3.a.text
+        board_title = preboard_title.strip()
     
-#     if container.div.time["datetime"] is not None:
-#         board_post_date = container.div.time["datetime"]
+    if container.div.time["datetime"] is not None:
+        board_post_date = container.div.time["datetime"]
     
-#     if container.a.text is not None:
-#         preboard_price = container.a.text
-#         board_price = preboard_price.strip()
+    if container.a.text is not None:
+        preboard_price = container.a.text
+        board_price = preboard_price.strip()
     
 
-#     if container.find('span', class_= 'result-hood') is not None:
-#         board_location_line = container.find('span', class_= 'result-hood')
-#         if board_location_line.text is not None:
-#             preboard_location = board_location_line.text
-#             board_location = preboard_location.strip()
+    if container.find('span', class_= 'result-hood') is not None:
+        board_location_line = container.find('span', class_= 'result-hood')
+        if board_location_line.text is not None:
+            preboard_location = board_location_line.text
+            board_location = preboard_location.strip()
     
-#     board_URLLine = container.find('h3', class_='result-heading')
-#     if board_URLLine.a["href"] is not None:
-#         preboard_URL = board_URLLine.a["href"]
-#         board_URL = preboard_URL.strip()
+    board_URLLine = container.find('h3', class_='result-heading')
+    if board_URLLine.a["href"] is not None:
+        preboard_URL = board_URLLine.a["href"]
+        board_URL = preboard_URL.strip()
     
-#     print("NAME: " + board_title)
-#     print("PRICE: " + board_price)
-#     print("LOCATION: " +board_location)
-#     print("URL Link: " + board_URL)
+    print("NAME: " + board_title)
+    print("PRICE: " + board_price)
+    print("LOCATION: " +board_location)
+    print("URL Link: " + board_URL)
     
-#     f.write(board_title.replace(",", "|") + "," + board_price.replace(",", "|") + "," + board_location.replace(",", "|") + "," + board_URL.replace(",", "|") +"\n")
-#     x = x+1
-#     if x<len(containers):
-#         container = containers[x]
-#     elif x>=len(containers):
-#         print("End Loop")
+    f.write(board_title.replace(",", "|") + "," + board_price.replace(",", "|") + "," + board_location.replace(",", "|") + "," + board_URL.replace(",", "|") +"\n")
+    x = x+1
+    if x<=int(result_range):
+        container = containers[x]
+    elif x>int(result_range):
+        print("End Loop")
 
-# f.close()
+f.close()
 
 
-
-# edit for the git tutorial test
-
-# editing this again
-#more changes and deleted the import language at the end
-
+#IMAGE SCRAPPING:
 #tryin to see if i can grab the images for the listsings:
 
-board_image_url = 'https://images.craigslist.org/{board_img_data_ids}_300x300.jpg'
+# board_image_url = 'https://images.craigslist.org/{board_img_data_ids}_300x300.jpg'
 
-# soup_img = soup(r.content, 'lxml')
+# # soup_img = soup(r.content, 'lxml')
 
-board_img_data_ids = container.a["data-ids"]
+# board_img_data_ids = container.a["data-ids"]
 
-board_img = container.a.img['src'] # saying that his is a Nonetype
+# board_img = container.a.img['src'] # saying that his is a Nonetype
 
-print(board_img)
+# print(board_img)
 
-print(board_img_data_ids) #the dataids are the only thing that changes for the image url.
+# print(board_img_data_ids) #the dataids are the only thing that changes for the image url.
+
+
 
 
 
